999458f1fbd5dba17cacfa2139a7fb88d2e69cc0
Evgeny Poberezkin <e.poberezkin@me.com>
2015-03-01 17:29:27 +0000

    fixed tests

--- tests/tests.js
+++ tests/tests.js
@@ -7,7 +7,7 @@ before(function (done) {
   var self = this;
   connect(function (client) {
     self.client = client
-    self.pgevents = PGEvents(client)
+    self.pgevents = PGEvents({ conString: 'postgres://localhost/test' })
     done()
   })
 })
@@ -16,90 +16,100 @@ describe('PG Events', function() {
 
   describe('.subcribe', function () {
     it('should subcribe to table', function (done) {
-      this.pgevents.subscribe('users', function (error) {
-        if (error) return done(error)
-        done()
-      })
-    })
-  })
+      var pgevents = this.pgevents;
+      pgevents.install(function() {
+        pgevents.subscribe('users', function (error) {
+          if (error) return done(error)
+          done();
+        });
+      });
+    });
+  });
 
   describe('table:insert', function () {
-
     before(function () {
-      var self = this
-      this.cache = {}
+      var self = this;
+      this.cache = {};
       this.pgevents.on('users:insert', function (msg) {
         self.cache = msg;
-      })
-    })
+      });
+    });
 
     it('should send JSON message',function (done) {
-      var self = this
-      this.client.query("insert into users (name) values ('pat')", function (error) {
-        if (error) return done(error)
+      var self = this;
+      this.client.query("insert into users (name) values ('pat')", function (error, data) {
+        if (error) return done(error);
 
-        validCache(self.cache)
-        assert.equal(self.cache.operation, 'insert', 'operation is insert')
+        setTimeout(function() {
+          validCache(self.cache);
+          assert.equal(self.cache.operation, 'INSERT');
 
-        done()
-      })
-    })
-
-  })
+          done();
+        }, 100); // timeout is needed when different client is used
+      });
+    });
+  });
 
   describe('table:update', function () {
 
     before(function (done) {
-      var self = this
-      this.cache = {}
+      var self = this;
       this.pgevents.on('users:update', function (msg) {
-        self.cache = msg
-      })
+        self.cache = msg;
+      });
 
-      this.client.query("insert into users (name) values ('pat')", done)
-    })
+      this.client.query("insert into users (name) values ('pat')", function(err) {
+        if (err) return done(err);
+        self.cache = {};
+        done();
+      });
+    });
 
     it('should send JSON message', function (done) {
-      var self = this
+      var self = this;
       this.client.query("UPDATE users SET name='cat' WHERE name='pat'", function (error) {
-        if (error) return done(error)
+        if (error) return done(error);
 
-        validCache(self.cache)
-        assert.equal(self.cache.operation, 'update', 'operation is insert')
-        assert.notDeepEqual(self.cache.old_data, {}, 'cache old_data is empty')
+        setTimeout(function() {
+          validCache(self.cache);
+          assert.equal(self.cache.operation, 'UPDATE');
+          assert.notDeepEqual(self.cache.old_data, {}, 'cache old_data is empty');
 
-        done()
-      })
-    })
-
-  })
+          done();
+        }, 100);
+      });
+    });
+  });
 
   describe('table:delete', function () {
     before(function (done) {
-      var self = this
-      this.cache = {}
+      var self = this;
       this.pgevents.on('users:delete', function (msg) {
-        self.cache = msg
-      })
-      this.client.query("insert into users (name) values ('pat')", done)
-    })
+        self.cache = msg;
+      });
+      this.client.query("insert into users (name) values ('pat')", function(err) {
+        if (err) return done(err);
+        self.cache = {};
+        done();
+      });
+    });
 
     it('should send JSON message', function (done) {
-      var self = this
+      var self = this;
       this.client.query("DELETE FROM users WHERE name='pat'", function (error) {
-        if (error) return done(error)
+        if (error) return done(error);
 
-        validCache(self.cache)
-        assert.equal(self.cache.operation, 'delete', 'operation is insert')
-        assert.notDeepEqual(self.cache.old_data, {}, 'cache old_data is empty')
+        setTimeout(function() {
+          validCache(self.cache);
+          assert.equal(self.cache.operation, 'DELETE');
+          assert.notDeepEqual(self.cache.old_data, {}, 'cache old_data is empty');
 
-        done()
-      })
-    })
-
-  })
-
-})
+          done();
+        }, 100);
+      });
+    });
+  });
+});
 
 /*
 *   Helpers
